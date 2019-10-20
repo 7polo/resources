bf221d9390f940e48c11e5d2866e0b362a014104
7polo <1368112354@qq.com>
2019-10-20 14:31:04 +0800

    update

--- push.sh
+++ push.sh
@@ -9,5 +9,7 @@ git commit -m "update"
 
 git push origin
 git push gitee
-
+http://apologizebao.gitee.io/resources/
 echo -e "\033[36mSuccess\033[0m"
+echo -e "\033[36mhttp://apologizebao.gitee.io/resources/\033[0m"
+echo -e "\033[36mhttp://apologizebao.gitee.io/resources/knowledge\033[0m"
