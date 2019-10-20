a507acef8d9143699bb8d7784f16e629f02ef590
7polo <1368112354@qq.com>
2019-10-20 14:28:50 +0800

    update

--- push.sh
+++ push.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
-echo "git fetch origin"
+echo -e "\033[36mStart.....\033[0m"
 git fetch origin 
-
-echo "git fetch gitee"
 git fetch gitee
 
-echo "git add & commit"
+echo -e "\033[36mAdd && Commit\033[0m"
 git add .
 git commit -m "update"
 
 git push origin
 git push gitee
 
-echo -e "\033[34mText\033[0m"
-echo -e  "\e[1;36mUpdate Success\e[0m"
+echo -e "\033[36mSuccess\033[0m"
